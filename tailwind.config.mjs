d348f058e325a08811dfea184ccec1748439b98c
Matthew Van Doff <mvandoff@gmail.com>
2024-06-23 11:42:43 -0400

    Set up basic Hero section

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,27 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				bg: {
+					50: '#FFFBF7',
+					100: '#F1EDE9',
+					200: '#DED6CF',
+					300: '#C7BBAE',
+					400: '#A7998B',
+					500: '#9B8F82',
+					600: '#7D7164',
+					700: '#635A4F',
+					800: '#4F4A45',
+					900: '#44403C',
+					1000: '#312E2B',
+				},
+				accent: {
+					400: '#7EB3BF',
+					500: '#6393A6',
+				},
+			},
+		},
 	},
 	plugins: [],
-}
+};
